685bd62fc35534a1b6f2c9378e7eee41f4e7ae90
Kalashnikov Egor <eskalashnikov@hse.ru>
2022-11-12 17:28:36 +0300

    Created test1

--- Vector/source/main.cpp
+++ Vector/source/main.cpp
@@ -1,19 +1,6 @@
-#include <iostream>
-#include "vector.h"
-
-void print(const MyVector& myvec) {
-	for (size_t i = 0; i < myvec.size(); ++i) {
-		std::cout << myvec[i] << ' ';
-	}
-	std::cout << '\n';
-}
+#include "tests.h"
 
 int main() {
-	const MyVector vec1(10, 3.14);
-	MyVector vec2;
-	vec2 = vec1;
-	vec2[3] = 10;
-	print(vec1);
-	print(vec2);	
+	test1();
 	return 0;
 }
