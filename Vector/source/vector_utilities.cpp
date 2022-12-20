d79becb14a127f99d00cdf52370141c1b92b1215
Artemiy Letunov <adletunov@edu.hse.ru>
2022-12-20 19:30:38 +0300

    first commit

--- Vector/source/vector_utilities.cpp
+++
@@ -1,9 +0,0 @@
-#include "vector_utilities.h"
-
-
-std::ostream& operator<<(std::ostream& out, const shrek::MyVector& vec) {
-	for (size_t i = 0; i < vec.size(); ++i) {
-		out << vec[i] << ' ';
-	}
-	return out;
-}
--- Vector/source/vector_utilities.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "vector.h"
-#include <ostream>
-
-std::ostream& operator<<(std::ostream& out, const shrek::MyVector& vec);
-
